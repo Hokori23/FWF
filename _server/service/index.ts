39d481526c8ed83c1dfb776536422263aa195fa8
Hokori23 <hokori23@qq.com>
2020-11-03 11:33:46 +0800

    fix: tinyfix

--- _server/service/index.ts
+++ _server/service/index.ts
@@ -2,3 +2,7 @@ import ApplierService from './ApplierService';
 import InterviewRecordService from './InterviewRecordService';
 
 export { ApplierService, InterviewRecordService };
+export default {
+  Applier: ApplierService,
+  InterviewRecord: InterviewRecordService
+};
